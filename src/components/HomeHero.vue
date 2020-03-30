25980e233e675d745ea8dc736a00b3cba1741e0f
Tyler Williams <tyler@ogdenstudios.xyz>
2020-03-30 15:16:40 -0600

    get demo component working

--- src/components/HomeHero.vue
+++ src/components/HomeHero.vue
@@ -10,7 +10,7 @@
         class="button"
       >{{ homeHero.buttonText}}</a>
     </div>
-    <img :src="homerHero.image" />
+    <img :src="homeHero.image" />
   </section>
 </template>
 
@@ -21,55 +21,50 @@ export default {
 </script>
 
 <style lang="scss">
-$HeroComponent__breakpoint: 1280px;
-
-.HeroComponent {
+.HomeHeroComponent {
   display: flex;
-  flex-direction: column;
-  //min-height: 66vh;
+  height: 66vh;
   overflow: hidden;
-  //margin-bottom: $half_space;
-  @media (max-width: 768px) {
-    //min-height: 75vh;
+  padding-bottom: 0;
+
+  @media (max-width: 1024px) {
+    flex-direction: column;
+    height: 75vh;
   }
+
   &__copy {
     align-self: flex-start;
     border-left: 2px solid $color_primary_brand;
     box-sizing: border-box;
     flex-shrink: 0;
-    margin: $half_space 0 $half_space $mobile_side_space;
+    margin: $half_space 0 0 $mobile_side_space;
     padding: 0 $mobile_side_space $half_space $mobile_side_space;
     padding-top: 0;
-    width: 1280px;
+    width: 500px;
     max-width: calc(100% - #{$half_space});
-    @media (min-width: 768px) {
-      box-sizing: content-box;
-      margin: $half_space 0 $base_space $base_side_space;
-      padding: 0 $base_side_space $half_space $base_side_space;
-    }
     p {
-      max-width: 500px;
+      padding-right: $base_space;
+    }
+
+    @media (min-width: 1440px) {
+      width: 690px;
+      box-sizing: content-box;
+      margin: $half_space 0 0 $base_side_space;
+      padding: 0 $base_side_space $half_space $base_side_space;
+      p {
+        padding-right: $double_space * 2;
+      }
     }
   }
-  &__image {
-    width: 100%;
-    height: 400px;
-    background-repeat: no-repeat;
-    background-size: cover;
-    background-position: center;
-    @media (max-width: 1200px) {
-      height: 250px;
-    }
-    @media (max-height: 900px) {
-      height: 250px;
-    }
-  }
+
   img {
-    width: 100%;
+    flex: 1;
     object-fit: cover;
-    object-position: center;
-    min-height: 200px;
-    max-height: 400px;
+    object-position: center right;
+    overflow: hidden;
+    @media (max-width: 1024px) {
+      object-position: center right;
+    }
   }
 }
 </style>
