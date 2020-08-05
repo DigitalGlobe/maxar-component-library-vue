04cff94c78a4933caabf8895d200e10d978c71f5
Tyler Williams <tyler@ogdenstudios.xyz>
2020-08-05 15:13:06 -0600

    touch up mapnav styles for reuse elsewhere

--- src/components/Maps/MapNav.vue
+++ src/components/Maps/MapNav.vue
@@ -1,20 +1,16 @@
 <template>
-  <nav class="applicationNavbar">
-    <div class="navbg"></div>
-    <router-link to="/" class="navbar__brand__wrapper">
-      <MaxarLogo class="navbar__brand" />
+  <nav class="mapNav">
+    <router-link to="/" class="mapNav__brand__wrapper">
+      <MaxarLogo class="mapNav__brand" />
     </router-link>
-    <div class="navbar">
-      <ul class="navbar__categories">
+    <div class="mapNav__navbar">
+      <ul class="mapNav__navbar__categories">
         <li
           v-for="link in navbar.links"
           :key="link.title"
-          class="navbar__categories__list-item"
+          class="mapNav__navbar__categories__list-item"
         >
-          <router-link
-            :to="link.path"
-            class="navbar__categories__header"
-          >{{link.title}}</router-link>
+          <router-link :to="link.path" class="mapNav__navbar__categories__header">{{link.title}}</router-link>
         </li>
       </ul>
     </div>
@@ -28,42 +24,27 @@ export default {
   components: {
     MaxarLogo,
   },
-  props: ["navbar"]
+  props: ["navbar"],
 };
 </script>
 
 <style lang="scss">
-nav {
+.mapNav {
+  background: $color_dark_grey;
   display: flex;
   flex-wrap: wrap;
   justify-content: space-between;
   padding: $half_space $base_side_space 0 $base_side_space;
   position: relative;
   z-index: 9999;
-
-  .navbar__brand__wrapper {
-    align-items: center;
-    display: flex;
-  }
 }
 
-.navbg {
-  background: $color_dark_grey;
-  box-sizing: border-box;
-  height: 100%;
-  width: 100%;
-  position: absolute;
-  top: 0;
-  left: 0;
-  z-index: -9;
+.mapNav__brand__wrapper {
+  align-items: center;
+  display: flex;
 }
 
-nav ul {
-  padding: 0;
-  list-style: none;
-}
-
-.navbar__brand {
+.mapNav__brand {
   height: 36px;
   max-width: 200px;
   width: 100%;
@@ -74,18 +55,25 @@ nav ul {
   }
 }
 
-.navbar__categories {
+.mapNav__navbar__categories {
+  align-content: center;
   display: flex;
+  list-style: none;
   margin: 0;
+  padding: 0;
 }
 
-.navbar__categories__header {
+.mapNav__navbar__categories__header {
   color: white;
   cursor: pointer;
   display: block;
+  font-size: 16px;
+  font-weight: 900;
+  line-height: 1.5;
   padding: $base_space;
   position: relative;
   transition: color 0.3s ease-in-out;
+  text-transform: uppercase;
   &:focus,
   &:hover {
     color: $color_primary_brand;
@@ -93,17 +81,17 @@ nav ul {
 }
 
 @media (max-width: 1024px) {
-  nav {
+  .mapNav {
     box-sizing: content-box;
     padding: $mobile_side_space;
   }
 
-  .navbar__brand {
+  .mapNav__brand {
     height: 17px;
     width: 94px;
   }
 
-  .navbar__categories__header {
+  .mapNav__navbar__categories__header {
     padding: 0 $mobile_side_space;
   }
 }
--- src/components/Navbar/Navbar.vue
+++ src/components/Navbar/Navbar.vue
@@ -79,7 +79,7 @@ export default {
 };
 </script>
 
-<style lang="scss" scoped>
+<style lang="scss">
 nav {
   display: flex;
   flex-wrap: wrap;
